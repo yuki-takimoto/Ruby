ebebaafe67a9312f6c3a10b29b6a48d419b996d5
DESKTOP-F274IKI\yuuki takimoto <karigurasi.yukun@gmail.com>
2021-05-20 20:34:30 +0900

    ちょっと進んだ

--- index.rb
+++ index.rb
@@ -1,5 +1,15 @@
-puts "good night"
-puts 10 / 4
-puts 10 / 4.0
-puts 10.0 / 4
-puts 10 **2
+# puts "good night"
+
+# puts 10 / 4
+# puts 10 / 4.0
+# puts 10.0 / 4
+# puts 10 **2
+
+# puts "入力してください"
+# text = gets
+# puts text + "が入力されました"
+
+puts "数値を入力"
+number = gets.to_i
+puts number**2
+
