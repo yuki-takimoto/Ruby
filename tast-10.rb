a5c44fb8d0b2a8aa4f1ce6f8ec493623208c194a
DESKTOP-F274IKI\yuuki takimoto <karigurasi.yukun@gmail.com>
2021-06-22 04:37:02 +0900

    Rubyテストの第10問 引数の渡し方がよくわからなかった

---
+++ tast-10.rb
@@ -0,0 +1,15 @@
+countries = ["japan","us","italy"]
+
+def greeting(???)
+  @country = ???
+  if @country = "japan"
+    puts "こんにちは"
+  elsif @country = "us"
+    puts "Hello"
+  elsif @country = "italy"
+    puts "ciao"
+  else
+    puts "対応する要素がありません"
+  end
+end
+
